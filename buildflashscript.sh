230fe818590e078c1340ee6e9e160abfa8ed245b
Anwesh Satpathy <anwesh1991@gmail.com>
2020-08-08 01:21:50 +0200

    Delete buildflashscript.sh

--- buildflashscript.sh
+++
@@ -1,8 +0,0 @@
-echo "Exporing gcc-arm path"
-export PATH="/opt/gcc-arm/bin:$PATH"
-echo "Making libopencm3 library and the src directory files"
-make
-echo "Generating bin files"
-arm-none-eabi-objcopy -Obinary src/sdtfttest.elf src/sdtfttest.bin
-echo "Flashing binary to the microcontroller"
-st-flash write src/colourlcdtest.bin 0x8000000
